1f6fcdcb63f926911351d5ce88810eb7bc76551b
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-05-03 12:47:15 +0500

    Added Layout and structured all by components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { Layout } from "antd";
+import AppHeader from "./components/layout/AppHeader";
+import AppSider from "./components/layout/AppSider";
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+  return (
+    <Layout>
+      <AppHeader />
+      <Layout>
+              <AppSider />
+              <AppContent />
+      </Layout>
+    </Layout>
+  );
 }
