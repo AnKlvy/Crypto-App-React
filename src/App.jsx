83831d6f7ab6c301b0a0c89075c66d9094c52f24
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-05-04 18:41:24 +0500

    separated fetching crypto in common context

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { Layout } from "antd";
 import AppHeader from "./components/layout/AppHeader";
 import AppSider from "./components/layout/AppSider";
 import AppContent from "./components/layout/AppContent";
+import {CryptoContextProvider} from "./context/crypto-context.jsx";
 
 export default function App() {
   return (
-    <Layout>
-      <AppHeader />
-      <Layout>
-              <AppSider />
-              <AppContent />
-      </Layout>
-    </Layout>
+    <CryptoContextProvider>
+        <Layout>
+            <AppHeader />
+            <Layout>
+                <AppSider />
+                <AppContent />
+            </Layout>
+        </Layout>
+    </CryptoContextProvider>
   );
 }
--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,38 +1,15 @@
 import {Card, Layout, Statistic, List, Typography, Spin, Tag} from "antd";
 import {ArrowUpOutlined, ArrowDownOutlined} from "@ant-design/icons";
-import {useEffect, useState} from "react";
-import {fakeFetchCrypto, fetchAssets} from "../../api.js";
-import {capitalize, percentDifference} from "../../utils.js";
+import {useContext} from "react";
+import {capitalize} from "../../utils.js";
+import CryptoContext from "../../context/crypto-context.jsx";
 
 const siderStyle = {
     padding: "1rem",
 };
 
 export default function AppSider() {
-    const [loading, setLoading] = useState(false);
-    const [crypto, setCrypto] = useState([]);
-    const [assets, setAssets] = useState([]);
-
-    useEffect(() => {
-        async function preload() {
-            setLoading(true);
-            const {result} = await fakeFetchCrypto();
-            const assets = await fetchAssets();
-            setAssets(assets.map((asset) => {
-                const coin = result.find((c) => c.id === asset.id);
-                return {
-                    grow: asset.price < coin.price,
-                    growPercent: percentDifference(asset.price, coin.price),
-                    totalAmount: asset.amount * coin.price,
-                    totalProfit: asset.amount * coin.price - asset.amount * asset.price, ...asset,
-                };
-            }));
-            setCrypto(result);
-            setLoading(false);
-        }
-
-        preload();
-    }, []);
+const {loading, assets} = useContext(CryptoContext)
 
     if (loading) {
         return <Spin fullscreen/>;
